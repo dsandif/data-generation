f7cec75ebe68c23762db2bb82bb7e1ae9cf891d4
Darien Sandifer <dariens@duck.com>
2023-02-13 15:28:33 -0600

    added created date

---
+++ SmartVault.Program/BusinessObjects/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SmartVault.Program.BusinessObjects
+{
+    public partial class User
+    {
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime CreatedOn { get; set; }
+    }
+}
